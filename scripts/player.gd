de88f536f972e6cb23d321e8d52475dbf41b5617
Jgong131 <76478194+Jgong131@users.noreply.github.com>
2025-01-26 20:29:09 -0800

    Character movement updates

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 @export var SPEED = 350.0
-@export var JUMP_VELOCITY = -400.0
+@export var JUMP_VELOCITY = -500.0
 
 @onready var pin_joint = $PinJoint2D
 @onready var grab_area = $Area2D
@@ -30,10 +30,13 @@ func _process(delta: float) -> void:
 			pin_joint.node_b = ""
 			
 
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
+		velocity += get_gravity() * delta * (2.5 if velocity.y >= 0 else 1)
 
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
@@ -41,6 +44,23 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
+	
+	#Flip Sprite Direction
+	if direction > 0:
+		animated_sprite_2d.flip_h = true
+	elif direction < 0: 
+		animated_sprite_2d.flip_h = false
+	
+	#Play animations
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+		
+	#Apply Movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
