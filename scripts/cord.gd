f6f8093d71d56f7893c44e438e45a76d956c7c00
Jordan S <jordy7789@hotmail.com>
2025-01-27 18:19:58 -0800

    add outlet and plug interaction

--- scripts/cord.gd
+++ scripts/cord.gd
@@ -2,20 +2,16 @@ extends Node2D
 
 @onready var max_cord_length = $CordAnchor.position.distance_to($Plug.position)
 
-var collision_enabled = false
-
 func _ready() -> void:
-	update_collision_mask()
+	pass
 
 func _process(delta: float) -> void:
-	if Input.is_action_just_pressed("rope_collision"):
-		collision_enabled = not collision_enabled
-		update_collision_mask()
+	pass
 
-func update_collision_mask() -> void:
+func update_collision_mask(collision: bool) -> void:
 	for segment in get_children():
-		if segment is RigidBody2D and segment.name != "Plug":
-			segment.set_collision_mask_value(1, collision_enabled)
+		if segment is RigidBody2D and segment.name != "Plug" and segment.name != "CordAnchor":
+			segment.set_collision_mask_value(1, collision)
 
 func get_max_cord_length() -> int:
 	return max_cord_length
--- scripts/plug.gd
+++ scripts/plug.gd
@@ -4,29 +4,16 @@ const STATE_NORMAL = "normal"
 const STATE_PLUGGED = "plugged"
 
 var state = null
-var connected_to = null
-
-@onready var joint = null
 
 func _ready() -> void:
 	state = STATE_NORMAL
 
 func _process(delta: float) -> void:
 	pass
+	
+func get_state() -> String:
+	return state
 
-func plug(outlet: Node2D) -> void:
-	if not connected_to:
-		connected_to = outlet
-		connected_to.set_state(STATE_PLUGGED)
-		global_position = connected_to.global_position
-		joint = PinJoint2D.new()
-		joint.node_a = get_path()
-		joint.node_a = connected_to.get_path()
-		joint.position = Vector2.ZERO
-		add_child(joint)
-
-func unplug() -> void:
-	if connected_to:
-		connected_to.status = STATE_NORMAL
-		joint.queue_free()
-		joint = null
+func set_state(new_state: String) -> void:
+	if new_state == STATE_NORMAL or new_state == STATE_PLUGGED:
+		state = new_state
